e8a35c6521cd9e3f268035b043c5c886dc890012
Tram Lai <tramlaisf@gmail.com>
2013-10-28 16:11:18 -0500

    Initial commit

---
+++ app/controllers/tables_controller.rb
@@ -0,0 +1,13 @@
+class TablesController < ApplicationController
+  def index
+    @title = "Reservester - Tables"
+  end
+
+  def show
+  end
+  
+  def welcome
+    @title = "Reservester - Welcome!"
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RestaurantApp::Application.config.secret_key_base = 'cf32fd5745a7bcd53c584dba510f8da81d8d0885d681dd71ddba5dc8afade3136eb45fd63756ecb9fbf4743f7862614db577580838135d064e00203ea9ce9d93'
