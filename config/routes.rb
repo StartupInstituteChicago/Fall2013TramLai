43a689793c2edc89f5f3d70c733dd3ee2f37a55f
Tram Lai <tramlaisf@gmail.com>
2013-11-18 10:43:02 -0600

    Create restaurant listing by caterogy

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,16 @@
   resources :restaurants do
     get 'guest', on: :collection
     resources :reservations
-  resources :categories
   end
+
+  resources :categories
+  
   root 'welcome#index'
 
   get "/owners/dashboard" => 'owners#dashboard'
   get "/owners/profile" => 'owners#profile'
-
-  get "/categories/index" => 'categories#index'
-
-  get "/categories/index/:category_id" => 'categories#show'
-  
-#  Prefix Verb   URI Pattern                                                 Controller#Action
+  end
+#     Prefix Verb   URI Pattern                                                 Controller#Action
 #           new_owner_session GET    /owners/sign_in(.:format)                                   devise/sessions#new
 #               owner_session POST   /owners/sign_in(.:format)                                   devise/sessions#create
 #       destroy_owner_session DELETE /owners/sign_out(.:format)                                  devise/sessions#destroy
@@ -41,14 +39,6 @@
 #                             PATCH  /restaurants/:restaurant_id/reservations/:id(.:format)      reservations#update
 #                             PUT    /restaurants/:restaurant_id/reservations/:id(.:format)      reservations#update
 #                             DELETE /restaurants/:restaurant_id/reservations/:id(.:format)      reservations#destroy
-#       restaurant_categories GET    /restaurants/:restaurant_id/categories(.:format)            categories#index
-#                             POST   /restaurants/:restaurant_id/categories(.:format)            categories#create
-#     new_restaurant_category GET    /restaurants/:restaurant_id/categories/new(.:format)        categories#new
-#    edit_restaurant_category GET    /restaurants/:restaurant_id/categories/:id/edit(.:format)   categories#edit
-#         restaurant_category GET    /restaurants/:restaurant_id/categories/:id(.:format)        categories#show
-#                             PATCH  /restaurants/:restaurant_id/categories/:id(.:format)        categories#update
-#                             PUT    /restaurants/:restaurant_id/categories/:id(.:format)        categories#update
-#                             DELETE /restaurants/:restaurant_id/categories/:id(.:format)        categories#destroy
 #                 restaurants GET    /restaurants(.:format)                                      restaurants#index
 #                             POST   /restaurants(.:format)                                      restaurants#create
 #              new_restaurant GET    /restaurants/new(.:format)                                  restaurants#new
@@ -57,6 +47,60 @@
 #                             PATCH  /restaurants/:id(.:format)                                  restaurants#update
 #                             PUT    /restaurants/:id(.:format)                                  restaurants#update
 #                             DELETE /restaurants/:id(.:format)                                  restaurants#destroy
+#                  categories GET    /categories(.:format)                                       categories#index
+#                             POST   /categories(.:format)                                       categories#create
+#                new_category GET    /categories/new(.:format)                                   categories#new
+#               edit_category GET    /categories/:id/edit(.:format)                              categories#edit
+#                    category GET    /categories/:id(.:format)                                   categories#show
+#                             PATCH  /categories/:id(.:format)                                   categories#update
+#                             PUT    /categories/:id(.:format)                                   categories#update
+#                             DELETE /categories/:id(.:format)                                   categories#destroy
+#                        root GET    /                                                           welcome#index
+#            owners_dashboard GET    /owners/dashboard(.:format)                                 owners#dashboard
+#              owners_profile GET    /owners/profile(.:format)                                   owners#profile
+#                             GET    /categories/index/:category_id(.:format)                    categories#show
+# unknownd8a25e969800:simple_restaurant tramlai$ rake routes
+#                      Prefix Verb   URI Pattern                                                 Controller#Action
+#           new_owner_session GET    /owners/sign_in(.:format)                                   devise/sessions#new
+#               owner_session POST   /owners/sign_in(.:format)                                   devise/sessions#create
+#       destroy_owner_session DELETE /owners/sign_out(.:format)                                  devise/sessions#destroy
+#              owner_password POST   /owners/password(.:format)                                  devise/passwords#create
+#          new_owner_password GET    /owners/password/new(.:format)                              devise/passwords#new
+#         edit_owner_password GET    /owners/password/edit(.:format)                             devise/passwords#edit
+#                             PATCH  /owners/password(.:format)                                  devise/passwords#update
+#                             PUT    /owners/password(.:format)                                  devise/passwords#update
+#   cancel_owner_registration GET    /owners/cancel(.:format)                                    devise/registrations#cancel
+#          owner_registration POST   /owners(.:format)                                           devise/registrations#create
+#      new_owner_registration GET    /owners/sign_up(.:format)                                   devise/registrations#new
+#     edit_owner_registration GET    /owners/edit(.:format)                                      devise/registrations#edit
+#                             PATCH  /owners(.:format)                                           devise/registrations#update
+#                             PUT    /owners(.:format)                                           devise/registrations#update
+#                             DELETE /owners(.:format)                                           devise/registrations#destroy
+#           guest_restaurants GET    /restaurants/guest(.:format)                                restaurants#guest
+#     restaurant_reservations GET    /restaurants/:restaurant_id/reservations(.:format)          reservations#index
+#                             POST   /restaurants/:restaurant_id/reservations(.:format)          reservations#create
+#  new_restaurant_reservation GET    /restaurants/:restaurant_id/reservations/new(.:format)      reservations#new
+# edit_restaurant_reservation GET    /restaurants/:restaurant_id/reservations/:id/edit(.:format) reservations#edit
+#      restaurant_reservation GET    /restaurants/:restaurant_id/reservations/:id(.:format)      reservations#show
+#                             PATCH  /restaurants/:restaurant_id/reservations/:id(.:format)      reservations#update
+#                             PUT    /restaurants/:restaurant_id/reservations/:id(.:format)      reservations#update
+#                             DELETE /restaurants/:restaurant_id/reservations/:id(.:format)      reservations#destroy
+#                 restaurants GET    /restaurants(.:format)                                      restaurants#index
+#                             POST   /restaurants(.:format)                                      restaurants#create
+#              new_restaurant GET    /restaurants/new(.:format)                                  restaurants#new
+#             edit_restaurant GET    /restaurants/:id/edit(.:format)                             restaurants#edit
+#                  restaurant GET    /restaurants/:id(.:format)                                  restaurants#show
+#                             PATCH  /restaurants/:id(.:format)                                  restaurants#update
+#                             PUT    /restaurants/:id(.:format)                                  restaurants#update
+#                             DELETE /restaurants/:id(.:format)                                  restaurants#destroy
+#                  categories GET    /categories(.:format)                                       categories#index
+#                             POST   /categories(.:format)                                       categories#create
+#                new_category GET    /categories/new(.:format)                                   categories#new
+#               edit_category GET    /categories/:id/edit(.:format)                              categories#edit
+#                    category GET    /categories/:id(.:format)                                   categories#show
+#                             PATCH  /categories/:id(.:format)                                   categories#update
+#                             PUT    /categories/:id(.:format)                                   categories#update
+#                             DELETE /categories/:id(.:format)                                   categories#destroy
 #                        root GET    /                                                           welcome#index
 #            owners_dashboard GET    /owners/dashboard(.:format)                                 owners#dashboard
 #              owners_profile GET    /owners/profile(.:format)                                   owners#profile
@@ -114,4 +158,4 @@
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
